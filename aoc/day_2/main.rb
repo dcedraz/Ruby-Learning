5eaf9b1b50f05145dc75c3221ce20f1a831ca520
dcedraz <dcedraz@salsify.com>
2023-12-05 22:18:49 +0000

    Day 2 part II

--- aoc/day_2/main.rb
+++ aoc/day_2/main.rb
@@ -6,7 +6,15 @@ require_relative 'game_set'
 file_data = File.readlines('input_file.txt')
 puts "There are #{file_data.count} rows to process"
 
-result = 0
+result_part_one = 0
+result_part_two = 0
+
+def part_two(game_sets)
+  min_greens = game_sets.map(&:greens).max
+  min_blues = game_sets.map(&:blues).max
+  min_reds = game_sets.map(&:reds).max
+  min_greens * min_blues * min_reds
+end
 
 file_data.each_with_index do |row, index|
   puts "Working on #{row}"
@@ -17,11 +25,13 @@ file_data.each_with_index do |row, index|
     end
   end
   game_sets.each(&:print_cubes)
+  result_part_two += part_two(game_sets)
 
   if game_sets.reject(&:valid).empty?
-    result += index + 1
-    puts "Added to total result: #{result}"
+    result_part_one += index + 1
+    puts "Added to total result_1: #{result_part_one}"
   end
 end
 
-puts result
+puts result_part_one
+puts result_part_two
