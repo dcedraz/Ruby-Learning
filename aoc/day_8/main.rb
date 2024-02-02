a29b07c416009fddce7663a76c5362db2781f8c0
dcedraz <dcedraz@salsify.com>
2024-02-02 21:18:21 +0000

    Day 8 part I - Refactor

--- aoc/day_8/main.rb
+++ aoc/day_8/main.rb
@@ -7,6 +7,23 @@ class DayEight
 
   def initialize(input_file: 'input_file.txt')
     @file_data = File.readlines(input_file).map(&:strip)
+    @instructions = parse_instructions
+    @hash_map = parse_hash_map
+  end
+
+  def parse_instructions
+    file_data.first.gsub('L', '0').gsub('R', '1').chars.map(&:to_i)
+  end
+
+  def parse_hash_map
+    Hash.new.tap do |hash_map|
+      file_data.each.with_index do |row, index|
+        next unless index > 1
+
+        elements_array = row.scan(/\w{3}/).map(&:to_sym)
+        hash_map[elements_array[0]] = [elements_array[1], elements_array[2]]
+      end
+    end
   end
 
   def perform
@@ -16,32 +33,19 @@ class DayEight
 
   def solve_part_one
     @result_part_one ||= 0
-    solve
-  end
-
-  def solve_part_two
-  end
-
-  def solve
-    @instructions ||= file_data.first.gsub('L', '0').gsub('R', '1').chars.map(&:to_i)
-    @hash_map ||= Hash.new.tap do |hash_map|
-      file_data.each.with_index do |row, index|
-        next unless index > 1
-
-        elements_array = row.scan(/\w{3}/).map(&:to_sym)
-        hash_map[elements_array[0]] = [elements_array[1], elements_array[2]]
-      end
-    end
     @element ||= :AAA
-    parse_instructions while @element != :ZZZ
+    parse_element while @element != :ZZZ
     result_part_one
   end
 
-  def parse_instructions
+  def parse_element
     instructions.each do |instruction|
       @result_part_one += 1
       @element = hash_map[@element][instruction]
     end
+  end
+
+  def solve_part_two
   end
 end
 
