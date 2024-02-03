c21c7ffec405f6e9f2e6597abcd583ada6143eac
dcedraz <dcedraz@salsify.com>
2024-02-03 20:03:55 +0000

    Day 8 part II

--- aoc/day_8/main.rb
+++ aoc/day_8/main.rb
@@ -3,12 +3,17 @@
 require 'pry'
 
 class DayEight
-  attr_reader :file_data, :instructions, :hash_map, :element, :result_part_one
+  attr_reader :file_data, :instructions, :hash_map, :element, :steps
+
+  ELEMENT_REGEX = /\w{3}/.freeze
+  ELEMENT_ENDS_WITH_A_REGEX = /\w{2}A/.freeze
+  ELEMENT_ENDS_WITH_Z_REGEX = /\w{2}Z/.freeze
 
   def initialize(input_file: 'input_file.txt')
     @file_data = File.readlines(input_file).map(&:strip)
     @instructions = parse_instructions
     @hash_map = parse_hash_map
+    @steps = 0
   end
 
   def parse_instructions
@@ -20,7 +25,7 @@ class DayEight
       file_data.each.with_index do |row, index|
         next unless index > 1
 
-        elements_array = row.scan(/\w{3}/).map(&:to_sym)
+        elements_array = row.scan(ELEMENT_REGEX).map(&:to_sym)
         hash_map[elements_array[0]] = [elements_array[1], elements_array[2]]
       end
     end
@@ -28,24 +33,31 @@ class DayEight
 
   def perform
     puts solve_part_one
-    # puts solve_part_two
+    puts solve_part_two
   end
 
   def solve_part_one
-    @result_part_one ||= 0
     @element ||= :AAA
     parse_element while @element != :ZZZ
-    result_part_one
+    steps
   end
 
   def parse_element
     instructions.each do |instruction|
-      @result_part_one += 1
+      @steps += 1
       @element = hash_map[@element][instruction]
     end
   end
 
   def solve_part_two
+    initial_elements = hash_map.keys.select { |element| element.match(ELEMENT_ENDS_WITH_A_REGEX) }
+    elements_steps = initial_elements.map do |curr_element|
+      @steps = 0
+      @element = curr_element
+      parse_element until @element.match(ELEMENT_ENDS_WITH_Z_REGEX)
+      steps
+    end
+    elements_steps.reduce(:lcm)
   end
 end
 
--- spec/aoc/day_8/main_spec.rb
+++ spec/aoc/day_8/main_spec.rb
@@ -3,13 +3,19 @@
 require_relative '../../../aoc/day_8/main'
 
 describe 'DayEight' do
-  let(:day_eight) { DayEight.new(input_file: '../../spec/aoc/day_8/input_file.txt') }
+  context 'Part I' do
+    let(:day_eight) { DayEight.new(input_file: '../../spec/aoc/day_8/input_file.txt') }
 
-  it 'works for part 1' do
-    expect(day_eight.solve_part_one).to eq(6)
+    it 'succeeds' do
+      expect(day_eight.solve_part_one).to eq(6)
+    end
   end
 
-  it 'works for part 2' do
-    # expect(day_eight.solve_part_two).to eq(5905)
+  context 'Part II' do
+    let(:day_eight) { DayEight.new(input_file: '../../spec/aoc/day_8/input_file_two.txt') }
+
+    it 'works for part 2' do
+      expect(day_eight.solve_part_two).to eq(6)
+    end
   end
 end
