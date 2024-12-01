8dea021849bb45fa9e24a57cc76e2b06e67ab9eb
dcedraz <dcedraz@salsify.com>
2024-12-01 14:10:51 +0000

    Solve day_1 part two

--- aoc/2024/day_1/main.rb
+++ aoc/2024/day_1/main.rb
@@ -3,15 +3,29 @@
 require 'pry'
 
 class DayOne
-  attr_reader :file_data, :left_instructions, :right_instructions, :instructions_difference
+  attr_reader :file_data, :left_instructions, :right_instructions, :instructions_difference,
+              :left_on_right_appearances, :similarities, :result_part_one, :result_part_two
 
   def initialize(input_file: 'input_file.txt')
     @file_data = File.readlines(input_file).map(&:strip)
     @left_instructions = []
     @right_instructions = []
     @instructions_difference = []
+    @left_on_right_appearances = []
+    @similarities = []
+    @result_part_one = 0
+    @result_part_two = 0
+  end
+
+  def perform
+    puts solve_part_one
+    puts solve_part_two
+  end
+
+  def solve_part_one
     parse_instructions
     calculate_difference
+    @result_part_one = instructions_difference.sum
   end
 
   def parse_instructions
@@ -31,20 +45,26 @@ class DayOne
     end
   end
 
-  def perform
-    puts solve_part_one
-    # puts solve_part_two
+  def solve_part_two
+    calc_left_on_right_appearances
+    calc_similarities
+    @result_part_two = similarities.sum
   end
 
-  def solve_part_one
-    instructions_difference.sum
+  def calc_left_on_right_appearances
+    right_appearances = right_instructions.each_with_object(Hash.new(0)) do |right, hash|
+      hash[right] += 1
+    end
+    @left_on_right_appearances = left_instructions.map do |left|
+      right_appearances[left]
+    end
   end
 
-  # def solve_part_two
-  #   left_instructions.zip(right_instructions).map do |left, right|
-  #     left * right
-  #   end.sum
-  # end
+  def calc_similarities
+    @similarities = left_instructions.zip(left_on_right_appearances).map do |left, appearances|
+      left * appearances
+    end
+  end
 end
 
 DayOne.new.perform
--- spec/aoc/2024/day_1/main_spec.rb
+++ spec/aoc/2024/day_1/main_spec.rb
@@ -7,6 +7,10 @@ describe 'DayOne' do
     # let(:day_one) { DayOne.new(input_file: '../../../spec/aoc/2024/day_1/input_file.txt') }
     let(:day_one) { DayOne.new(input_file: 'input_file.txt') }
 
+    before do
+      day_one.perform
+    end
+
     # input:
     # 3   4
     # 4   3
@@ -27,16 +31,37 @@ describe 'DayOne' do
       expect(day_one.instructions_difference).to eq([2, 1, 0, 1, 2, 5])
     end
 
-    it 'succeeds' do
-      expect(day_one.solve_part_one).to eq(11)
+    it 'succeeds by summarizing the differences' do
+      expect(day_one.result_part_one).to eq(11)
     end
   end
 
-  # context 'Part II' do
-  #   let(:day_one) { DayOne.new(input_file: '../../spec/aoc/2024/day_1/input_file_two.txt') }
-  #
-  #   it 'works for part 2' do
-  #     expect(day_one.solve_part_two).to eq(6)
-  #   end
-  # end
+  context 'Part II' do
+    # let(:day_one) { DayOne.new(input_file: '../../spec/aoc/2024/day_1/input_file_two.txt') }
+    let(:day_one) { DayOne.new(input_file: 'input_file.txt') }
+
+    before do
+      day_one.perform
+    end
+
+    # input:
+    # 3   4
+    # 4   3
+    # 2   5
+    # 1   3
+    # 3   9
+    # 3   3
+
+    it 'finds how many times the left instructions appears in the right instructions' do
+      expect(day_one.left_on_right_appearances).to eq([0, 0, 3, 3, 3, 1])
+    end
+
+    it 'multiples the left instructions by the number of appearances' do
+      expect(day_one.similarities).to eq([0, 0, 9, 9, 9, 4])
+    end
+
+    it 'succeeds by multiplying the left instructions by the number of appearances' do
+      expect(day_one.result_part_two).to eq(31)
+    end
+  end
 end
