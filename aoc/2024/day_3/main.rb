61035b06f494b35909ffadc08499896c35a62b09
dcedraz <dcedraz@salsify.com>
2024-12-03 09:34:32 +0000

    Solve day_3 part two

--- aoc/2024/day_3/main.rb
+++ aoc/2024/day_3/main.rb
@@ -3,13 +3,21 @@
 require 'pry'
 
 class DayThree
-  attr_reader :file_data, :muls
+  attr_reader :file_data, :muls, :muls_donts_and_dos, :enabled_muls
 
   MUL_REGEX = /mul\((\d+),(\d+)\)/
+  MUL_DONT_DO_REGEX = /do\(\)|don't\(\)|mul\(\d+,\d+\)/
 
   def initialize(input_file: 'input_file.txt')
     @file_data = File.readlines(input_file).map(&:strip)
     @muls = []
+    @muls_donts_and_dos = []
+    @enabled_muls = []
+  end
+
+  def perform
+    solve_part_one
+    solve_part_two
   end
 
   def solve_part_one
@@ -32,4 +40,37 @@ class DayThree
     end.reduce(:+)
   end
 
+  def solve_part_two
+    parse_muls_donts_and_dos
+    parse_enabled_muls
+    puts calculate_enabled_muls
+  end
+
+  def parse_muls_donts_and_dos
+    @muls_donts_and_dos = file_data.flat_map do |line|
+      line.scan(MUL_DONT_DO_REGEX)
+    end
+  end
+
+  def parse_enabled_muls
+    mul_enabled = true
+
+    muls_donts_and_dos.each do |mul|
+      if mul == "don't()"
+        mul_enabled = false
+      elsif mul == "do()"
+        mul_enabled = true
+      elsif mul_enabled
+        @enabled_muls << mul.scan(MUL_REGEX).flatten.map(&:to_i)
+      end
+    end
+  end
+
+  def calculate_enabled_muls
+    enabled_muls.map do |pair|
+      pair.reduce(:*)
+    end.reduce(:+)
+  end
 end
+
+DayThree.new.perform
--- spec/aoc/2024/day_3/main_spec.rb
+++ spec/aoc/2024/day_3/main_spec.rb
@@ -18,10 +18,30 @@ describe 'DayThree' do
     end
 
     it 'multiplies all mul pairs and returns the sum' do
-      expect(day_three.calculate_muls).to eq(161)
+      expect(day_three.solve_part_one).to eq(161)
     end
   end
 
   context 'Part II' do
+    let(:day_three) { DayThree.new(input_file: 'input_file_part_two.txt') }
+
+    before do
+      day_three.solve_part_two
+    end
+
+    # input:
+    # xmul(2,4)&mul[3,7]!^don't()_mul(5,5)+mul(32,64](mul(11,8)undo()?mul(8,5))
+
+    it "finds all the muls including don't() and do()" do
+      expect(day_three.muls_donts_and_dos).to eq(["mul(2,4)", "don't()", "mul(5,5)", "mul(11,8)", "do()", "mul(8,5)"])
+    end
+
+    it 'finds all the enabled muls' do
+      expect(day_three.enabled_muls).to eq([[2, 4], [8, 5]])
+    end
+
+    it 'multiplies all mul pairs and returns the sum' do
+      expect(day_three.calculate_enabled_muls).to eq(48)
+    end
   end
 end
